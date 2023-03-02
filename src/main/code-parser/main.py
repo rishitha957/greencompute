c8aeeb678bff3a426960e736ca7289df15103ad4
Rishitha Kalicheti <cs17b014@iittp.ac.in>
2023-03-02 19:30:33 +0530

    added parsing utils

--- src/main/code-parser/main.py
+++ src/main/code-parser/main.py
@@ -1,7 +1,9 @@
 import json
 import os
+from parse import *
 
 dataset_location = os.getcwd()+'/src/main/resources/dataset.json'
 f = open(dataset_location)
 data = json.load(f)
-print(data)
+repo_url = data[1]['repositories'][0]
+parse_repo(repo_url)
